d81af22e3ca7a6e702f714b3779eb369ab90d2e0
Rafal-Maliszewski <rafal.p.maliszewski@gmail.com>
2024-11-21 14:38:07 +0100

    Update 1.03
    Decreased execution time for more diversed wallets (+15 coins) by using 3 free API's.

--- crypto_tracker.py
+++ crypto_tracker.py
@@ -22,24 +22,37 @@ def read_currencies_from_file(filename):
     return currencies, folder_path
 
 
-def get_cryptocurrency_price_coingecko(symbol, response):
+def get_cryptocurrency_price_coingecko(symbol, response, loop):
+    excluded_coins=["agility","artificial-general-intelligence"]
     if response.status_code == 200:
         data = response.json()
         #with open('coins_list.json', 'w') as json_file:
         #    json.dump(data, json_file, indent=4)
         coin_id = None
         for coin in data:
-            if coin["symbol"] == symbol and coin['id'] != "agility":
-                coin_id = coin["id"]
-                break
+            if coin["symbol"] == symbol and coin['id'] not in excluded_coins:
+                if coin["symbol"]=="agi" and coin["id"]!="delysium":
+                    #print(coin["id"]+" rejected")
+                    pass
+                else:
+                    coin_id = coin["id"]
+                    break
         if coin_id:
             price_url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin_id}&vs_currencies=usd"
             price_response = requests.get(price_url)
             code = price_response.status_code
-            while code == 429:
-                time.sleep(2)
-                price_response = requests.get(price_url)
-                code = price_response.status_code
+            if code == 429:
+                if loop==False:
+                    return 429
+                elif loop==True:
+                    while code==429:
+                        retry_after = price_response.headers.get('Retry-After')
+                        print ("Too many request to serwer. Retrying after: " + str(retry_after) + " sec")
+                        for remaining in range(int(retry_after), 0, -10):
+                            print(f"{remaining}...")
+                            time.sleep(10) 
+                        price_response = requests.get(price_url)
+                        code = price_response.status_code
             if  code == 200:
                 price_data = price_response.json()
                 return price_data[coin_id]["usd"]
@@ -47,6 +60,7 @@ def get_cryptocurrency_price_coingecko(symbol, response):
                 print("Failed to fetch price data "+str(price_url)+'  '+str(price_response))
                 return None
         else:
+            print("Coin id error")
             return None
     else:
         print("Coingecko bad response: "+str(response.status_code))
@@ -62,7 +76,7 @@ def get_cryptocurrency_price_paprika(symbol):
             if currency['symbol'] == symbol:
                 price = currency['quotes']['USD']['price']
                 return price
-        print(f"Cryptocurrency with symbol {symbol} not found")
+        #print(f"Cryptocurrency with symbol {symbol} not found")
         return None
     else:
         print("Failed to fetch data")
@@ -78,12 +92,17 @@ def get_price(folder_path):
     url_gecko = "https://api.coingecko.com/api/v3/coins/list"
     response_gecko = requests.get(url_gecko)
     currencies_fetched=0
-    def exec_info(currencies_fetched):
+    exec_list=[]
+    def exec_info(currencies_fetched,exec_list,currencies_count):
         exec_proc= currencies_fetched/currencies_count
         if (not (0.23 < exec_proc < 0.27) and not (0.48 < exec_proc < 0.52) and not (0.73 < exec_proc < 0.77) and exec_proc != 1):
             pass
         else:
-            print(str(round(exec_proc*100))+'%')
+            exec_print=str((round(exec_proc*100/5))*5)+'%'#print only 25,50,75,100 % values
+            if exec_print not in exec_list: #do not print twice 
+                print (exec_print)
+                return exec_print
+
     with open(filename, 'w') as file:
         for i in currencies:
             file.write(f"{i}\n")
@@ -95,36 +114,32 @@ def get_price(folder_path):
                 price = data['price']
                 value=round(float(currencies[i])*float(price),2)
                 total_value+=value
-                #print(f"{data['symbol']} price is {price}") 
-                #print(f"{data['symbol']} value is {value} \n")
                 file.write(f"quantity: {currencies[i]}\n")
                 file.write(f"price: {price}\n")
                 file.write(f"value: {value}\n\n")
                 currencies_fetched+=1
-                #print (currencies_fetched)
-                exec_info(currencies_fetched)
+                unique=exec_info(currencies_fetched,exec_list,currencies_count)
+                if unique!=None:
+                    exec_list.append(unique)
+
             except KeyError: #if coin not found try Coingecko API
                 coin_symbol=(i.split('USDT')[0]).lower()
-                price=get_cryptocurrency_price_coingecko(coin_symbol,response_gecko)
-                if price == None:#if coin not found try Paprika API
-                    coin_symbol=coin_symbol.upper()
-                    price=get_cryptocurrency_price_paprika(coin_symbol)
-                if price == None:
-                    value="Error: coin not found!"
-                else:
+                coin_symbol_pap=coin_symbol.upper()
+                price=get_cryptocurrency_price_paprika(coin_symbol_pap)
+                if price==None:#if coin not found try BINANCE API
+                    price=get_cryptocurrency_price_coingecko(coin_symbol,response_gecko,True)
+                if price not in [None,429]:
                     value=round(float(currencies[i])*float(price),2)
                     total_value+=value
-                #print(f"{coin_symbol} price is {price}")
-                #print(f"{coin_symbol} value is {value} \n")
                 file.write(f"quantity: {currencies[i]}\n")
                 file.write(f"price: {price}\n")
                 file.write(f"value: {value}\n\n")
                 currencies_fetched+=1
-                #print (currencies_fetched)
-                exec_info(currencies_fetched)
+                unique=exec_info(currencies_fetched,exec_list,currencies_count)
+                if unique!=None:
+                    exec_list.append(unique)
             except Exception as e:
                 file.write(f"Error: {e}\n\n")
-        #print(round(total_value,2))
         file.write(f"total_value: {round(total_value,2)}")
         return filedate, filename
 
@@ -147,10 +162,8 @@ def read_data(filename):
                 except ValueError:
                     value = 0
                 coin_values[coin_symbol] = value
-
             else:
-                pass     
-    
+                pass        
             total_value = float(lines[-1].split(":")[1])
     return coin_values, total_value
 
@@ -158,7 +171,6 @@ def create_wheel_chart(coin_values, total_value, filedate, folder_path):
     sorted_coin_values = dict(sorted(coin_values.items(), key=lambda item: item[1], reverse=True))  
     labels = sorted_coin_values.keys()
     sizes = [(sorted_coin_values[symbol] / total_value) * 100 for symbol in sorted_coin_values]
-    
     fig, ax = plt.subplots(figsize=(15, 15))
     ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=100)
     ax.axis('equal') 
